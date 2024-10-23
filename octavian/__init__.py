58088717f0fb2fcbdf10b501bb6d49a8c39c21dd
jszpila314 <jakub.szpila314@gmail.com>
2024-10-24 01:11:43 +0200

    Initial version

---
+++ octavian/__init__.py
@@ -0,0 +1,1 @@
+from octavian.octavian import OCTAVIAN
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='octavian',
+  version=0.1,
+  url='https://github.com/jszpila314/octavian',
+  author='Jakub Szpila',
+  author_email='jakub.szpila314@gmail.com',
+  packages=find_packages(),
+  python_requires='>=3.11',
+  install_requires=[
+    'numpy', 'pandas', 'scipy', 'scikit-learn', 'astropy', 'unyt', 'h5py'
+  ]
+)

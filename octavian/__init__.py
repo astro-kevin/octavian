437640f47ebe5f9af224173acb6f2af0f7379f45
astro-kevin <kevin.lo@ed.ac.uk>
2026-06-05 10:59:19 +0100

    Add halo-finder progenitor linking

--- octavian/__init__.py
+++ octavian/__init__.py
@@ -1,4 +1,5 @@
 from octavian.run import run
 from octavian.mpirun import mpirun
 from octavian.halo_filter import filter_snapshot
+from octavian.progen import progen
 from octavian.utils import merge_catalogues
